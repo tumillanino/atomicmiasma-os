276544129cb49a7b69f8e9b94da054d2e9bacce2
tumillanino <anthony.tumia@live.com.au>
2026-03-13 11:30:37 +1100

    added dank material shell to load with niri as default

--- files/scripts/install-lazyvim.sh
+++ files/scripts/install-lazyvim.sh
@@ -2,6 +2,5 @@
 set -oue pipefail
 
 # Clone LazyVim starter into /etc/skel to apply to all new users
-mv ~/.config/nvim{,.bak}
 git clone https://github.com/LazyVim/starter /etc/skel/.config/nvim
 rm -rf /etc/skel/.config/nvim/.git
