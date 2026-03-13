f07333074022b7928d525016af92fdad3a4164f8
tumillanino <anthony.tumia@live.com.au>
2026-03-13 11:04:41 +1100

    added shell update

--- files/scripts/install-lazyvim.sh
+++ files/scripts/install-lazyvim.sh
@@ -2,6 +2,6 @@
 set -oue pipefail
 
 # Clone LazyVim starter into /etc/skel to apply to all new users
-mkdir -p /etc/skel/.config/nvim
+mv ~/.config/nvim{,.bak}
 git clone https://github.com/LazyVim/starter /etc/skel/.config/nvim
 rm -rf /etc/skel/.config/nvim/.git
--- files/scripts/update-shell.sh
+++ files/scripts/update-shell.sh
@@ -1,1 +1,7 @@
-usermod -s /usr/bin/zsh $USER
+
+#!/usr/bin/env
+set -euo pipefail
+
+for user in $(cut -f1 -d: /etc/passwd); do
+    chsh -s $(which zsh) $user
+done
